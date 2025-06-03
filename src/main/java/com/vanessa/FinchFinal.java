49e98a5b8c7a4e852feeb2923d15b4a8bccef1da
Vanesssarive321 <vrivera0091@mymail.lausd.net>
2025-06-03 10:38:58 -0700

    created the FinchFinal.java file

---
+++ src/main/java/com/vanessa/FinchFinal.java
@@ -0,0 +1,20 @@
+package com.vanessa;
+
+import com.birdbrain.Finch;
+
+public class FinchFinal {
+    public static void main(String[] args) {
+        Finch bird = new Finch();
+
+
+        // WWhat do you want your bird to do?
+
+
+
+
+
+
+        bird.stopAll();
+        bird.disconnect();
+    }
+}
